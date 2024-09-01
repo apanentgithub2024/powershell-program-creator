0a1eac0ccc2d133b471e9547425ecb678015d837
apanentgithub2024 <165437400+apanentgithub2024@users.noreply.github.com>
2024-09-01 19:15:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,3 +14,121 @@ button2.onclick = function() {
 	button2.disabled = true
 	button1.disabled = false
 }
+// FUNCTIONALITY
+const newWindowButton = document.createElement("button"), newCode = document.createElement("button"), pieces = [
+	{
+		opcode: "messageBox",
+		type: 1,
+		text: "msg. box with title <A> text <B> options <C> icon <D>",
+		displayAs: "show a message box",
+		isHidden: false,
+		arguments: {
+			A: {
+				type: "string",
+				defaultValue: "Greeting"
+			},
+			B: {
+				type: "string",
+				defaultValue: "Hello, world!"
+			},
+			C: {
+				type: "dropdown",
+				defaultValue: "Yes/No",
+				items: {
+					"Yes/No": "YesNo",
+					"OK": "OK",
+					"OK/Cancel": "OKCancel",
+					"Yes/No/Cancel": "YesNoCancel"
+				}
+			},
+			D: {
+				type: "dropdown",
+				defaultValue: "Information",
+				items: {
+					"Warning": "Warn",
+					"Information": "Info",
+					"Error": "Error",
+					"Question": "Request"
+				}
+			}
+		}
+	}
+]
+category1.appendChild(newWindowButton)
+category2.appendChild(newCode)
+newWindowButton.textContent = "New Window Display"
+newCode.textContent = "Add New Code Piece"
+let currentCode = []
+newCode.onclick = function() {
+	newCode.disabled = true
+	const sel = document.createElement("select")
+	category2.appendChild(sel)
+	for (const block of pieces) {
+		if (!block.isHidden) {
+			const option = document.createElement("option")
+			option.value = block.opcode
+			option.textContent = block.displayAs
+			sel.appendChild(option)
+		}
+	}
+	sel.oninput = function() {
+		const blockToMimic = pieces.find(i => i.opcode == sel.value)
+		const block = document.createElement("div")
+		const matches = blockToMimic.text.replace(/[\w\s]+|<([\w\s]+)>/gi, function(match, multiple) {
+			let input
+			if (multiple) {
+				switch (blockToMimick.arguments[multiple].type) {
+					case "string":
+						const inp = document.createElement("input")
+						block.appendChild(inp)
+						input = inp
+						break
+					case "multistring":
+						const inp = document.createElement("textarea")
+						block.appendChild(inp)
+						input = inp
+						break
+					case "number":
+						const inp = document.createElement("input")
+						inp.type = "number"
+						block.appendChild(inp)
+						input = inp
+						break
+					case "color":
+						const inp = document.createElement("input")
+						inp.type = "color"
+						block.appendChild(inp)
+						input = inp
+						break
+					case "dropdown":
+						const inp = document.createElement("select")
+						const keys = Object.keys(blockToMimick.arguments[multiple].items)
+						for (const option of keys) {
+							const op = document.createElement("option")
+							op.textContent = option
+							op.value = blockToMimick.arguments[multiple].items[option]
+							inp.appendChild(op)
+						}
+						block.appendChild(inp)
+						input = inp
+						break
+					default:
+						const inp = document.createElement("div")
+						inp.style = "background-color: AAAAAA; border-radius: 6px; display: inline"
+						block.appendChild(inp)
+						input = inp
+				}
+			} else {
+				const text = document.createElement("a")
+				text.textContent = match
+				block.appendChild(text)
+				input = null
+			}
+			if (input !== null) {
+				
+			}
+		})
+		sel.remove()
+		newCode.disabled = false
+	}
+}
