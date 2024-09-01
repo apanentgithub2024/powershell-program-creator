2ee51ebe37e0c8988a0c2c968266f9e3da471030
apanentgithub2024 <165437400+apanentgithub2024@users.noreply.github.com>
2024-09-01 12:25:30 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+// UI
+const button1 = document.getElementById("windowDisplaysCategory"), button2 = document.getElementById("programCategory"), category1 = document.getElementById("windowDisplays"), category2 = document.getElementById("program")
+category2.style.display = "none"
+button1.disabled = true
+button1.onclick = function() {
+	category1.style.display = "block"
+	category2.style.display = "none"
+	button1.disabled = true
+	button2.disabled = false
+}
+button2.onclick = function() {
+	category2.style.display = "block"
+	category1.style.display = "none"
+	button2.disabled = true
+	button1.disabled = false
+}
